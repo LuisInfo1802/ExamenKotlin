71d78697f747e87091f76820602e19f478f98d62
LuisInfo1802 <luissanchezinfo18@gmail.com>
2023-06-02 12:04:36 -0700

    Se agrego una nueva Actividad CuentaBancoActivity y se le hizo el diseño

---
+++ app/src/main/java/com/example/examenkotlin/CuentaBancoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.examenkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CuentaBancoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cuenta_banco)
+    }
+}
